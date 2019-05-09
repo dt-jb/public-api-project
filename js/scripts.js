e446aef6e5898687c914fd691daa2b1e373e7f7f
dt_jb <jbrown8013@gmail.com>
2019-05-09 17:33:55 +1000

    adds modal window functionality, search functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 
 const howManyPeople = 12;
-const randomPeopleUrl = `https://randomuser.me/api/?results=${howManyPeople}`;
+const latinCharNationalities =  'au,br,ca,ch,de,dk,es,fi,fr,gb,ie,no,nl,nz,us';
+const randomPeopleUrl = `https://randomuser.me/api/?nat=${latinCharNationalities}&results=${howManyPeople}`;
 const galleryDiv = document.getElementById('gallery');
 let employeeGallery = [];
 
@@ -28,154 +29,104 @@ function makeGalleryHTML(data){
     `;
     employeeGallery.push(employee);
   });
+  return data;
 }
 
 function modalEvents(data) {
   const cards = document.querySelectorAll('#card');
-  cards.forEach(card => {
+  cards.forEach( (card, i) => {
     card.addEventListener('click', (event) => {
-      modalHTML(event);
+      modalHTML(data, i);
+      //console.log(i);
     });
   });
 }
 
-function modalHTML(event) {
-      //const div = document.getElementById('card').parentNode;
-      const modalDiv = document.createElement('div');
-      galleryDiv.appendChild(modalDiv);
-      const employeeGalleryModal = console.log(employeeGallery.slice());
-      const firstName = event.currentTarget.children[1].children[0].textContent.split(' ')[0];
-      /*console.log(employeeGalleryModal
-        .filter(employee => {
-          employee.name.first === event.currentTarget.children[1].children[0].textContent.split(' ')[0];
-        }));*/
-      console.log(employeeGallery.forEach(item => {
-          if(item.name.first === firstName){
-            return employeeGallery.indexOf(item);
-          };
-        )};
-      //console.log(employeeGallery.forEach(employee => console.log(employee.name.first)));
-      //console.log(employee)
-      //console.log(event.currentTarget.children[1].children[0].textContent.split(' ')[0]);
-
-      //console.log(employeeGallery.(event.currentTarget));
-      //console.log(employeeGallery.findIndex( e => {
-      //  event.currentTarget.textContent.includes(employeeGallery.name.first);
-      //}));
-      //console.log(employeeGallery.indexOf(event.target.textContent));
-      //console.log(card);
-      //modalDiv.innerHTML = `<h1>${data.name.first}</h1>`;
-
-  //return data;
-}
-/*
-modalDiv.innerHTML = `
-  <div class="modal-container" display = "none">
-      <div class="modal">
-          <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-          <div class="modal-info-container" id="div-modal">
-              <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-              <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
-              <p class="modal-text">${data.email}</p>
-              <p class="modal-text cap">${datalocation.city}</p>
-              <hr>
-              <p class="modal-text">${data.phone}</p>
-              <p class="modal-text">${data.location.street}, ${data.location.city}, ${data.location.state} ${data.location.postcode}</p>
-              <p class="modal-text">${data.dob}Birthday: 10/21/2015</p>
-          </div>
-      </div>
-  </div>
-  `;
-*/
-
-/*
-function addModals() {
+function modalHTML(data, i) {
   const modalDiv = document.createElement('div');
-  //const referenceNode = document.querySelector('script');
-  //referenceNode.parentNode.insertBefore(modalDiv, referenceNode.nextSibling);
-  gallery.appendChild(modalDiv);
-  modalDiv.innerHTML =  `
-    <div class="modal-container">
+  galleryDiv.appendChild(modalDiv);
+  modalDiv.innerHTML = `
+    <div class="modal-container" display = "none">
         <div class="modal">
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-            <div class="modal-info-container">
-                <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-                <h3 id="name" class="modal-name cap">name</h3>
-                <p class="modal-text">email</p>
-                <p class="modal-text cap">city</p>
+            <div class="modal-info-container" id="div-modal">
+                <img class="modal-img" src=${employeeGallery[i].picture.medium} alt="profile picture">
+                <h3 id="name" class="modal-name cap">${employeeGallery[i].name.first} ${employeeGallery[i].name.last}</h3>
+                <p class="modal-text">${employeeGallery[i].email}</p>
+                <p class="modal-text cap">${employeeGallery[i].location.city}</p>
                 <hr>
-                <p class="modal-text">(555) 555-5555</p>
-                <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                <p class="modal-text">Birthday: 10/21/2015</p>
+                <p class="modal-text">${employeeGallery[i].phone}</p>
+                <p class="modal-text">${employeeGallery[i].location.street}, ${employeeGallery[i].location.city}, ${employeeGallery[i].location.state} ${employeeGallery[i].location.postcode}</p>
+                <p class="modal-text">${employeeGallery[i].dob.date}</p>
             </div>
         </div>
     </div>
     `;
+  const closeBtn = document.getElementById('modal-close-btn');
+  //console.log(closeBtn);
+  closeBtn.addEventListener('click', () => {
+    modalDiv.parentElement.removeChild(modalDiv);
+  });
 }
-*/
 
-/*
-fetch(randomPeopleUrl)
-  .then(res => res.json())
-  .then(data => makeGalleryHTML(data.results))
-  .then(modalEvents);
-*/
+function employeeSearch (data) {
+  const searchContainerDiv = document.getElementsByClassName('search-container');
+  const searchDiv = document.createElement('form');
+  searchContainerDiv[0].appendChild(searchDiv);
+  searchDiv.innerHTML = `
+    <form action="#" method="get">
+        <input type="search" id="search-input" class="search-input" placeholder="Search...">
+        <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
+    </form>
+  `;
+
+  const searchInput = document.getElementById('search-input');
+  const searchForm = searchInput.parentNode;
+  searchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const searchArr = employeeGallery.filter(employee => {
+      return searchInput.value.toLowerCase() === employee.name.last || searchInput.value.toLowerCase() === employee.name.first;
+    });
+    //hides gallery div (#card) and shows search results html.
+    const searchResultsDiv = document.createElement('div');
+    const body = document.querySelector('body');
+    galleryDiv.style.display = "none";
+    //galleryDiv.style.visibility = "hidden";
+    body.appendChild(searchResultsDiv);
+    searchResultsDiv.innerHTML = `
+      <div class="card" id="card">
+          <div class="card-img-container">
+              <img class="card-img" src=${searchArr[0].picture.medium} alt="profile picture">
+          </div>
+          <div class="card-info-container">
+              <h3 id="name" class="card-name cap">${searchArr[0].name.first} ${searchArr[0].name.last}</h3>
+              <p class="card-text">${searchArr[0].email}</p>
+              <p class="card-text cap">${searchArr[0].location.city}, ${searchArr[0].location.state}</p>
+          </div>
+      </div>
+    `;
+    searchDiv.style.display = "none";
+    searchDiv.style = "";
+    const backBtn = document.createElement('button');
+    body.appendChild(backBtn);
+    backBtn.innerHTML = `
+      <input type="submit" value="back" id="serach-submit" class="search-submit">
+    `;
+    backBtn.addEventListener('click', () => {
+      galleryDiv.style.display = "block";
+      galleryDiv.style = "";
+      searchDiv.style.display = "block";
+      searchDiv.style = "";
+      searchResultsDiv.parentNode.removeChild(searchResultsDiv);
+      backBtn.parentNode.removeChild(backBtn);
+      searchInput.value = "";
+    });
+
+  });
+}
 
 
 fetchData(randomPeopleUrl)
   .then(data => makeGalleryHTML(data.results))
+  .then(data => employeeSearch(data))
   .then(modalEvents);
-  //.then(addModals);
-/*
-function fetchData(url){
-    return fetch(url)
-            .then(res => res.json());
-}
-*/
-
-//card.addEventListener('click', () => {console.log(card)})
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-Modal markup:
-
-You can use the commented out markup below as a template
-for your modal, but you must use JS to create and append
-it to `body`.
-
-IMPORTANT: Altering the arrangememnt of the markup and the
-attributes used may break the styles or functionality.
-
-<div class="modal-container">
-    <div class="modal">
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        <div class="modal-info-container">
-            <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-            <h3 id="name" class="modal-name cap">name</h3>
-            <p class="modal-text">email</p>
-            <p class="modal-text cap">city</p>
-            <hr>
-            <p class="modal-text">(555) 555-5555</p>
-            <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-            <p class="modal-text">Birthday: 10/21/2015</p>
-        </div>
-    </div>
-
-    // IMPORTANT: Below is only for exceeds tasks
-    <div class="modal-btn-container">
-        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-        <button type="button" id="modal-next" class="modal-next btn">Next</button>
-    </div>
-</div>
-*/
